2b6531577ef8c1b7f1e51f639ae6aaf7aac06069
Marc Steven Clemen <mackyclemen@gmail.com>
2022-03-31 06:32:39 +0800

    Changed FAB implementation to a normal button

--- app/src/main/java/edu/dogfood/finals/MainActivity.kt
+++ app/src/main/java/edu/dogfood/finals/MainActivity.kt
@@ -6,41 +6,36 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
-import android.view.View
+import android.widget.Button
 import androidx.appcompat.widget.Toolbar
-import androidx.coordinatorlayout.widget.CoordinatorLayout
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val contextView: CoordinatorLayout = findViewById(R.id.main_coordinator_layout)
-
         val toolbar: Toolbar = findViewById(R.id.main_toolbar)
         setSupportActionBar(toolbar)
 
-        val fab: FloatingActionButton = findViewById(R.id.main_add)
-        fab.setOnClickListener {
-            Snackbar.make(contextView, "You clicked on a FAB!", Snackbar.LENGTH_LONG)
-                .show()
+        // Compute GWA button
+        val actionComputeBtn: Button = findViewById(R.id.main_btn_compute)
+        actionComputeBtn.setOnClickListener {
+            val intent = Intent(this, ConversionActivity::class.java)
+            startActivity(intent)
         }
 
-        val score = 4.0
-        val calfab: FloatingActionButton = findViewById(R.id.main_calculate)
-        calfab.setOnClickListener {
-            val i = Intent(this, ResultActivity::class.java)
-            i.putExtra(resources.getString(R.string.key_calculate), score)
-            startActivity(i)
+        // Conversion Table button
+        val actionShowConversionBtn: Button = findViewById(R.id.main_btn_conversion)
+        actionShowConversionBtn.setOnClickListener {
+            // TODO Link the table activity to this button
+            // val intent = Intent(this, ConversionActivity::class.java)
+            // startActivity(intent)
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.main_activity, menu)
+        inflater.inflate(R.menu.activity_main, menu)
         return true
     }
 
@@ -49,12 +44,6 @@ class MainActivity : AppCompatActivity() {
             R.id.main_menu_about -> {
                 val aboutIntent = Intent(this, AboutActivity::class.java)
                 startActivity(aboutIntent)
-                true
-            }
-
-            R.id.main_menu_conversion -> {
-                val conversionIntent = Intent(this, ConversionActivity::class.java)
-                startActivity(conversionIntent)
                 true
             }
 
